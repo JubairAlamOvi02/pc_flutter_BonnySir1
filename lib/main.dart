b217c8051ce5a174ae2fa27a587eed31020a994f
Jubair Alam Ovi <61586952+JubairAlamOvi02@users.noreply.github.com>
2022-04-16 17:44:19 +0600

    pc FBS 01 init

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+//import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(primarySwatch: Colors.cyan),
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'By The Name Of ALLAH',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+              color: Colors.white, fontSize: 25, fontStyle: FontStyle.italic),
+        ),
+      ),
+      body: Center(
+          child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Text(
+            'By the name of ALLAH',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 18),
+          ),
+          const Text(
+            'He is the only one',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 18, fontStyle: FontStyle.italic),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Cancel'),
+              ),
+              const SizedBox(width: 10,),
+
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Submit'),
+              ),
+            ],
+          ),
+        ],
+      )),
+    ),
+  ));
+}
